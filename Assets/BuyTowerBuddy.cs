b88519b23ffbef77bc5c8e566efa55868cd7a9f1
Enes Samet Karabacak <enes_samet10@hotmail.com>
2025-09-28 01:17:05 +0300

    UfakFix

--- Assets/BuyTowerBuddy.cs
+++ Assets/BuyTowerBuddy.cs
@@ -18,10 +18,13 @@ public class BuyTowerBuddy : MonoBehaviour
         if(gameManager.money >= buddyCost)
         {
             GameObject yeniKale = Instantiate(towerBuddy, nextKale.position, Quaternion.identity) as GameObject;
-            if (nextKale.position.y == -2.4) yeniKale.GetComponent<SpriteRenderer>().sortingOrder = -2;
-            if (nextKale.position.y == .2) yeniKale.GetComponent<SpriteRenderer>().sortingOrder = -3;
-            if (nextKale.position.y == 2.8) yeniKale.GetComponent<SpriteRenderer>().sortingOrder = -4;
-            if (nextKale.position.y == 5.4) yeniKale.GetComponent<SpriteRenderer>().sortingOrder = -5;
+            float y = nextKale.position.y;
+
+            if (Mathf.Approximately(y, -2.4f)) yeniKale.GetComponent<SpriteRenderer>().sortingOrder = -2;
+            if (Mathf.Approximately(y, 0.2f)) yeniKale.GetComponent<SpriteRenderer>().sortingOrder = -3;
+            if (Mathf.Approximately(y, 2.8f)) yeniKale.GetComponent<SpriteRenderer>().sortingOrder = -4;
+            if (Mathf.Approximately(y, 5.4f)) yeniKale.GetComponent<SpriteRenderer>().sortingOrder = -5;
+
             gameManager.buddyShooting.Add(yeniKale.GetComponentInChildren<Shooting>());
             nextKale.position = new Vector2(nextKale.position.x, nextKale.position.y + 2.6f);
             gameManager.money -= buddyCost;
