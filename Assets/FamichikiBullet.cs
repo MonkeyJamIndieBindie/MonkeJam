fa0b79c0486a7e2439f2aa26a461eb08ae9040d7
savuzyelim <yavuzselimdinc687@gmail.com>
2025-09-29 01:15:49 +0300

    chris mermisi daha hissiyat

--- Assets/FamichikiBullet.cs
+++ Assets/FamichikiBullet.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FamichikiBullet : MonoBehaviour
@@ -7,32 +6,65 @@ public class FamichikiBullet : MonoBehaviour
     public float hitDammage;
     bool canGetBig = true;
 
+    [Header("Impact VFX")]
+    [SerializeField] ParticleSystem impactVfxPrefab;  //  buraya prefab'ýný sürükle
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
-            if(canGetBig == true) StartCoroutine(GetBig());
-            if(collision.gameObject.GetComponent<EnemyWalk>() != null)
+            if (canGetBig) StartCoroutine(GetBig());
+            var ew = collision.gameObject.GetComponent<EnemyWalk>();
+            if (ew != null)
             {
-                collision.gameObject.GetComponent<EnemyWalk>().enemySpeed -= 1;
-                collision.gameObject.GetComponent<EnemyWalk>().hit = true;
+                ew.enemySpeed -= 1;
+                ew.hit = true;
             }
         }
+
         if (collision.gameObject.CompareTag("Zemin"))
         {
-            if (canGetBig == true) StartCoroutine(GetBig());
+            if (canGetBig) StartCoroutine(GetBig());
         }
     }
 
     IEnumerator GetBig()
     {
         canGetBig = false;
-        transform.GetComponent<Rigidbody2D>().gravityScale = 0;
-        transform.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
-        GetComponent<CircleCollider2D>().radius = 2;
-        transform.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 0);
-        yield return new WaitForSeconds(.5f);
+
+        var rb = GetComponent<Rigidbody2D>();
+        if (rb)
+        {
+            rb.gravityScale = 0;
+            rb.velocity = Vector2.zero;
+            rb.angularVelocity = 0f;
+            rb.isKinematic = true;
+        }
+
+        var cc = GetComponent<CircleCollider2D>();
+        if (cc) cc.radius = 2f;
+
+        var sr = GetComponent<SpriteRenderer>();
+        if (sr) sr.enabled = false;
+
+        // >>> ÇARPTIÐI NOKTADA VFX’i çalýþtýr
+        PlayImpactVfx(transform.position);
+
+        yield return new WaitForSeconds(0.5f);
         Destroy(gameObject);
     }
 
+    void PlayImpactVfx(Vector3 pos)
+    {
+        if (impactVfxPrefab == null) return;
+
+        // Instantiate + Play
+        var ps = Instantiate(impactVfxPrefab, pos, Quaternion.identity);
+        ps.Play();
+
+        // Kendi kendini temizlemesi için yok et
+        var main = ps.main;
+        float life = main.duration + main.startLifetime.constantMax;
+        Destroy(ps.gameObject, life + 0.1f);
+    }
 }
